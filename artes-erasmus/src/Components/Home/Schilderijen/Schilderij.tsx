4661feef8b7ff39eb8afbb05bd8028b0b2569cc6
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 11:33:38 +0200

    Img voor eindwerk

---
+++ artes-erasmus/src/Components/Home/Schilderijen/Schilderij.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nachtwacht from "../../../Assets/Nachtwacht.jpg"
+
+function Schilderij(){
+    return(
+        <div className="schilderijCont">
+            <img src={Nachtwacht} alt="eindwerk-template"></img>
+        </div>
+    )
+}
+
+export default Schilderij
