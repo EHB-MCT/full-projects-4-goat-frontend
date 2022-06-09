8089fac56e05f177dbc97a268790b5adf8126129
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 19:53:01 +0200

    Dummy data beschrijving

--- artes-erasmus/src/Components/Detail/Beschrijving.tsx
+++ artes-erasmus/src/Components/Detail/Beschrijving.tsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+function Beschrijving(){
+    return(
+        <div className="descriptionCont">
+            <div className="mainInfo">
+                <h5 className="titleProjectD"></h5>
+                <h6 className="authorProjectD"></h6>
+            </div>
+            <div className="sideInfo">
+                <p className="descriptionProject"></p>
+            </div>
+
+        </div>
+    )
+}
+export default Beschrijving
