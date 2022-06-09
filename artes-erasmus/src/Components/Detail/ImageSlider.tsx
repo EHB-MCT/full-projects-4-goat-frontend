57f6f17a32e0d26055c76a0ea1d6ebff8bc321c3
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 15:20:48 +0200

    Import of images badges

---
+++ artes-erasmus/src/Components/Detail/ImageSlider.tsx
@@ -0,0 +1,5 @@
+import Web from "../../Assets/web-badge-2.png"
+import Motion from "../../Assets/motion-badge-2.png"
+import Mobile from "../../Assets/mobile-badge-2.png"
+import Digital from "../../Assets/digital-badge-2.png"
+import Game from "../../Assets/game-badge-2.png"
