d9cdd5750796935a5f314316766baaa090513ecd
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 21:08:28 +0200

    Use modal

--- artes-erasmus/src/Components/Home/Schilderijen/Schilderijen.tsx
+++ artes-erasmus/src/Components/Home/Schilderijen/Schilderijen.tsx
@@ -2,9 +2,19 @@ import { useEffect, useState } from "react";
 import Schilderij from "./Schilderij";
 import Tag from "./Tag";
 import "../../../scss/schilderijen.scss"
+import DetailPage from "../../Detail/DetailPage";
 
 function Schilderijen(){
     const [tag, setTags] = useState<any>([])
+
+    const [isOpen, setIsOpen] = useState(false);
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
 
     useEffect (() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,15 +24,35 @@ function Schilderijen(){
 
     const test = tag
     return (
-        <div className="artGallery">
-            {test.map((titles: any) => {
-                return (
-                    <div className="artCont">
-                        <Schilderij/>
-                        <Tag title={titles.title} author={titles.id}/>
+        <div className="modalCont">
+            {isOpen && (
+                <>
+                <div className="overlay"></div>
+                <div className="modal">
+                    <div className="modalHeader">
+                    {/* <h2>Modal Title</h2> */}
+                        <button onClick={closeModal} className="close-button">&times;</button>
                     </div>
-                )
-            })}
+                    <main className="modal__main">
+                        <DetailPage/>
+                    {/* <p>Some content here!</p> */}
+                    </main>
+                </div>
+                </>
+            )}
+
+            <div className="artGallery">
+                {test.map((titles: any) => {
+                    return (
+                        <button className="button" onClick={openModal}>
+                            <div className="artCont">
+                                <Schilderij/>
+                                <Tag title={titles.title} author={titles.id}/>
+                            </div>
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     ) 
 }
