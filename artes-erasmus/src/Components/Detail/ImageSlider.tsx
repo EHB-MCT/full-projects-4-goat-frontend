fe69eb7936a9277240440572aebb658495c399a6
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 15:35:36 +0200

    Html styling

--- artes-erasmus/src/Components/Detail/ImageSlider.tsx
+++ artes-erasmus/src/Components/Detail/ImageSlider.tsx
@@ -3,3 +3,23 @@ import Motion from "../../Assets/motion-badge-2.png"
 import Mobile from "../../Assets/mobile-badge-2.png"
 import Digital from "../../Assets/digital-badge-2.png"
 import Game from "../../Assets/game-badge-2.png"
+import Nachtwacht from "../../Assets/Nachtwacht.jpg"
+import "../../scss/imageslider.scss"
+
+function ImageSlider(){
+    return(
+        <div className="imageCont">
+            <img src={Nachtwacht} alt="image-project" />
+
+            <div className="previewImagesCont">
+                <img src={Nachtwacht} alt="image-project" />
+                <img src={Nachtwacht} alt="image-project" />
+                <img src={Nachtwacht} alt="image-project" />
+                <img src={Nachtwacht} alt="image-project" />
+                <img src={Nachtwacht} alt="image-project" />
+            </div>
+        </div>
+    )
+}
+
+export default ImageSlider
