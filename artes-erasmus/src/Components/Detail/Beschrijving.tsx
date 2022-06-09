32244b80affd2f3939ac3b48cf9aa1af26e5f51d
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 19:53:18 +0200

    Extra Dummy data

--- artes-erasmus/src/Components/Detail/Beschrijving.tsx
+++ artes-erasmus/src/Components/Detail/Beschrijving.tsx
@@ -4,11 +4,11 @@ function Beschrijving(){
     return(
         <div className="descriptionCont">
             <div className="mainInfo">
-                <h5 className="titleProjectD"></h5>
-                <h6 className="authorProjectD"></h6>
+                <h5 className="titleProjectD">Lincoln</h5>
+                <h6 className="authorProjectD">Henk De Steen</h6>
             </div>
             <div className="sideInfo">
-                <p className="descriptionProject"></p>
+                <p className="descriptionProject">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam consequatur iure tempora aliquam facilis quidem obcaecati doloribus voluptatem voluptas, eum exercitationem alias voluptate distinctio excepturi enim recusandae dolores error?</p>
             </div>
 
         </div>
