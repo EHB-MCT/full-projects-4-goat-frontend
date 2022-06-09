b0c4c480ef39e79a30ddc45cd8ce8e3b523109bb
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 14:32:42 +0200

    Static data in tags

---
+++ artes-erasmus/src/Components/Home/Schilderijen/Schilderijen.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Schilderij from "./Schilderij";
+import Tag from "./Tag";
+
+function Schilderijen(){
+    const [tag, setTags] = useState<any>([])
+
+    useEffect (() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => setTags(data));
+    }, [])
+
+    const test = tag
+    return (
+        <div>
+            {test.map((titles: any) => {
+                console.log("Titles", titles.title);
+                return (
+                    <div className="artCont">
+                        <Schilderij/>
+                        <Tag title={titles.title} author={titles.id}/>
+                    </div>
+                )
+            })}
+        </div>
+    ) 
+}
+
+export default Schilderijen
