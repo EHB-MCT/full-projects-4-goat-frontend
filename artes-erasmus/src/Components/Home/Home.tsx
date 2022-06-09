fd3935c05e24e4e4705b435593546e990ecf8bcd
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-09 21:08:39 +0200

    Showing back homepage

--- artes-erasmus/src/Components/Home/Home.tsx
+++ artes-erasmus/src/Components/Home/Home.tsx
@@ -1,14 +1,14 @@
 import Schilderijen from "./Schilderijen/Schilderijen";
 import TopNav from "../algemeen/topNav";
 import DetailPage from "../Detail/DetailPage";
+import "../../scss/Modal.scss"
 
 function Home(){
 
     return (
         <div>
             <TopNav/>
-            {/* <Schilderijen/> */}
-            <DetailPage/>
+            <Schilderijen/>
         </div>
     )
 }
