320a8973e8365522269d5bf71313e8f5f82c7ccb
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-09 15:10:30 +0200

    updated home.tsx

--- artes-erasmus/src/Components/Home/Home.tsx
+++ artes-erasmus/src/Components/Home/Home.tsx
@@ -1,10 +1,13 @@
 import Schilderijen from "./Schilderijen/Schilderijen";
+import TopNav from "../algemeen/topNav";
 
 function Home(){
 
     return (
         <div>
+            <TopNav/>
             <Schilderijen/>
+            
         </div>
     )
 }
