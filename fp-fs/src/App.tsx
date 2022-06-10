33a08445f79fb55635e524d8769274ab36fccf0f
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-10 14:38:06 +0200

    Home Component inladen

--- fp-fs/src/App.tsx
+++ fp-fs/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './SCSS/App.scss';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
