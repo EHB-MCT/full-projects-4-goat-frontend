71ab5126cef68880d71e18c89b9abf4d3a486420
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-10 15:18:02 +0200

    Classname for image

--- fp-fs/src/Components/Home/Nav.tsx
+++ fp-fs/src/Components/Home/Nav.tsx
@@ -1,10 +1,11 @@
 import Logo from '../../Assets/Final-Show-Logo.png'
 import '../../SCSS/Button.scss'
+import '../../SCSS/Nav.scss'
 
 function Nav(){
     return(
         <div className="navContainer">
-            <img src={Logo} alt="logo-final-show" />
+            <img className='logoFinal' src={Logo} alt="logo-final-show" />
             <button className='uploadenBtn btn'>Uploaden</button>
             <button className='stemmenBtn btn'>Stemming</button>
             <button className='searchBtn btn'>Search</button>
