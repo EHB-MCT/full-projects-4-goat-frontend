6c64f664cae678d2f1afc0bef225edfb01d59127
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-10 14:48:31 +0200

    Buttons for Nav

--- fp-fs/src/Components/Home/Nav.tsx
+++ fp-fs/src/Components/Home/Nav.tsx
@@ -1,9 +1,13 @@
 import Logo from '../../Assets/Final-Show-Logo.png'
+import '../../SCSS/Button.scss'
 
 function Nav(){
     return(
         <div className="navContainer">
             <img src={Logo} alt="logo-final-show" />
+            <button className='uploadenBtn btn'>Uploaden</button>
+            <button className='stemmenBtn btn'>Stemming</button>
+            <button className='searchBtn btn'>Search</button>
         </div>
     )
 }
