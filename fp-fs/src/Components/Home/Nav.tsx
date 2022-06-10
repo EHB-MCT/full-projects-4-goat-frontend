f51801cddafc96f9e0907ee6a41226ebf88ae0cf
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-10 14:38:21 +0200

    Image logo in nav

--- fp-fs/src/Components/Home/Nav.tsx
+++ fp-fs/src/Components/Home/Nav.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Logo from '../../Assets/Final-Show-Logo.png'
 
 function Nav(){
     return(
         <div className="navContainer">
-
+            <img src={Logo} alt="logo-final-show" />
         </div>
     )
 }
