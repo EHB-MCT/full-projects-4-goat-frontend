f4050efba95a71876edcaf0d612bda2d5791ab0d
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-10 16:30:07 +0200

    Extra nav voor mobile

--- fp-fs/src/Components/Home/Nav.tsx
+++ fp-fs/src/Components/Home/Nav.tsx
@@ -1,4 +1,5 @@
-import Logo from '../../Assets/Final-Show-Logo.png'
+import { useState } from 'react';
+import Logo from '../../Assets/Final-Show-Logo.svg'
 import '../../SCSS/Button.scss'
 import '../../SCSS/Nav.scss'
 
@@ -6,9 +7,13 @@ function Nav(){
     return(
         <div className="navContainer">
             <img className='logoFinal' src={Logo} alt="logo-final-show" />
-            <button className='uploadenBtn btn'>Uploaden</button>
-            <button className='stemmenBtn btn'>Stemming</button>
-            <button className='searchBtn btn'>Search</button>
+            <div className='ButtonsNav'>
+                <button className='uploadenBtn btn'>Uploaden</button>
+                <button className='stemmenBtn btn'>Stemming</button>
+                <button className='searchBtn btn'>Search</button>
+            </div>
+            <div className='hamburgerNav'>
+            </div>
         </div>
     )
 }
