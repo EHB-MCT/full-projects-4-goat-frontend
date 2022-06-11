d84da14860e1ea88fc6f03bb82380c89b2ca67a7
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-11 17:21:48 +0200

    Import scss footer + scss footerImg

--- fp-fs/src/Components/Standard/Footer.tsx
+++ fp-fs/src/Components/Standard/Footer.tsx
@@ -1,9 +1,10 @@
-import Erasmus from '../../Assets/logo-ehb-small.svg'
+import Erasmus from '../../Assets/logo-ehb-small.svg';
+import '../../SCSS/Footer.scss';
 
 function Footer(){
     return(
         <div className="footerContainer">
-            <img src={Erasmus} alt="erasmus-logo-black" />
+            <img className='footerImg' src={Erasmus} alt="erasmus-logo-black" />
         </div>
     )
 }
