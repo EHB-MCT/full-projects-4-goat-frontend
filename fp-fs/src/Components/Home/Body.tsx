47e7f1725aa19738c120edeb303679413fc7aeca
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-11 17:34:58 +0200

    Body component

---
+++ fp-fs/src/Components/Home/Body.tsx
@@ -0,0 +1,13 @@
+import "../../SCSS/Body.scss"
+
+function Body(){
+    return(
+        <div className="bodyContainer">
+            <span id="blue"></span>
+            <span id="red"></span>
+            <span id="orange"></span>
+            <span id="yellow"></span>
+        </div>
+    )
+}
+export default Body
--- fp-fs/src/Components/Home/Home.tsx
+++ fp-fs/src/Components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Nav from "../Standard/Nav";
 import Footer from "../Standard/Footer";
-import "../../SCSS/Home.scss"
+import "../../SCSS/Home.scss";
+import Body from "./Body";
 
 
 function Home(){
     return(
         <div className="homeContainer">
             <nav><Nav/></nav>
+            <Body/>
             <footer><Footer/></footer>
         </div>
     )
