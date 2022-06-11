d272532c9faf2d2a61f1fbfbe1055568036b7983
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-11 17:22:12 +0200

    From div to section Nav

--- fp-fs/src/Components/Standard/Nav.tsx
+++ fp-fs/src/Components/Standard/Nav.tsx
@@ -6,14 +6,14 @@ import '../../SCSS/Nav.scss'
 function Nav(){
     return(
         <div>
-            <div className="navContainer">
-                <img className='logoFinal' src={Logo} alt="logo-final-show" />
+            <section className="navContainer">
+                <img className='logoFinalGroot' src={Logo} alt="logo-final-show" />
                 <div className='ButtonsNav'>
                     <button className='uploadenBtn btn'>Uploaden</button>
                     <button className='stemmenBtn btn'>Stemming</button>
                     <button className='searchBtn btn'>Search</button>
                 </div>
-            </div>
+            </section>
             <section className="top-nav">
                 <div className='logoFinal' >
                     <img src={Logo} alt="logo-final-show" />
@@ -29,41 +29,6 @@ function Nav(){
                 </ul>
             </section>
         </div>
-        // <div>
-        //     <div className="navContainer">
-        //         <img className='logoFinal' src={Logo} alt="logo-final-show" />
-        //         <div className='ButtonsNav'>
-        //             <button className='uploadenBtn btn'>Uploaden</button>
-        //             <button className='stemmenBtn btn'>Stemming</button>
-        //             <button className='searchBtn btn'>Search</button>
-        //         </div>
-        //     </div>
-        //     <div className='hamburgerNav'>
-        //         <a href="" className="logo">CSS Nav</a>
-        //         <input className="menu-btn" type="checkbox" id="menu-btn" />
-        //         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-        //         <ul className="menu">
-        //             <li><a href="#work">Our Work</a></li>
-        //             <li><a href="#about">About</a></li>
-        //             <li><a href="#careers">Careers</a></li>
-        //             <li><a href="#contact">Contact</a></li>
-        //         </ul>
-        //         {/* <section className="top-nav">
-        //             <div className='logoFinal' >
-        //                 <img src={Logo} alt="logo-final-show" />
-        //             </div>
-        //             <input id="menu-toggle" type="checkbox" />
-        //             <label className='menu-button-container' htmlFor="menu-toggle">
-        //                 <div className='menu-button'></div>
-        //             </label>
-        //             <ul className="menu">
-        //                 <li>Uploaden</li>
-        //                 <li>Stemming</li>
-        //                 <li>Search</li>
-        //             </ul>
-        //         </section> */}
-        //     </div>
-        // </div>
     )
 }
 
