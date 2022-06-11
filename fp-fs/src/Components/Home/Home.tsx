9834a65d21985e6f3b61d744b59c21cb4193c216
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-11 10:21:53 +0200

    Hamburger menu mobile

--- fp-fs/src/Components/Home/Home.tsx
+++ fp-fs/src/Components/Home/Home.tsx
@@ -4,7 +4,7 @@ import Nav from "./Nav"
 function Home(){
     return(
         <div className="homeContainer">
-            <nav><Nav/></nav>
+            <Nav/>
         </div>
     )
 }
--- fp-fs/src/Components/Home/Nav.tsx
+++ fp-fs/src/Components/Home/Nav.tsx
@@ -5,14 +5,30 @@ import '../../SCSS/Nav.scss'
 
 function Nav(){
     return(
-        <div className="navContainer">
-            <img className='logoFinal' src={Logo} alt="logo-final-show" />
-            <div className='ButtonsNav'>
-                <button className='uploadenBtn btn'>Uploaden</button>
-                <button className='stemmenBtn btn'>Stemming</button>
-                <button className='searchBtn btn'>Search</button>
+        <div>
+            <div className="navContainer">
+                <img className='logoFinal' src={Logo} alt="logo-final-show" />
+                <div className='ButtonsNav'>
+                    <button className='uploadenBtn btn'>Uploaden</button>
+                    <button className='stemmenBtn btn'>Stemming</button>
+                    <button className='searchBtn btn'>Search</button>
+                </div>
             </div>
             <div className='hamburgerNav'>
+                <section className="top-nav">
+                    <div className='logoFinal' >
+                        <img src={Logo} alt="logo-final-show" />
+                    </div>
+                    <input id="menu-toggle" type="checkbox" />
+                    <label className='menu-button-container' htmlFor="menu-toggle">
+                        <div className='menu-button'></div>
+                    </label>
+                    <ul className="menu">
+                        <li>Uploaden</li>
+                        <li>Stemming</li>
+                        <li>Search</li>
+                    </ul>
+                </section>
             </div>
         </div>
     )
