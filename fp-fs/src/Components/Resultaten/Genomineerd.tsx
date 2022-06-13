ef95e14fa1ab96cbe4ce420bca097098833937c2
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-13 17:11:44 +0200

    Default components results/genomineerd/winnaars

---
+++ fp-fs/src/Components/Resultaten/Genomineerd.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Genomineerd(){
+    return(
+        <div className="genomineerdContainer">
+            <h1>Genomineerd</h1>
+        </div>
+    )
+}
+
+export default Genomineerd
---
+++ fp-fs/src/Components/Resultaten/Results.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Results() {
+
+  return (
+    <div className="resContainer">
+        <h1>Hallo</h1>
+    </div>
+  )
+}
+export default Results
