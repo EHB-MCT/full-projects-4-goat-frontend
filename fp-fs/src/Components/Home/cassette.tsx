438dfa8a82025041aabd7ad6ea6e43303e1110f5
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-13 16:29:22 +0200

    Added randomized cassettes and css (mobile)

--- fp-fs/src/Components/Home/cassette.tsx
+++ fp-fs/src/Components/Home/cassette.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
 import '../../SCSS/cassette.scss'
+import side_1 from '../../Assets/side_1.png'
+import side_2 from '../../Assets/side_2.png'
+import side_3 from '../../Assets/side_3.png'
 
 export default function Cassette(){
+
+    const [cassette, setCassette] = useState(String)
+
+    useEffect(() => {
+
+        var number = Math.floor(Math.random() * (3 - 1 + 1)) + 1
+
+        if(number === 1){
+            setCassette(side_1)
+        } else if (number === 2) {
+            setCassette(side_2)
+        } else if (number === 3) {
+            setCassette(side_3)
+        }
+    }, [])
+
+
     return (
         <div className="cassette">
-             <p>test</p>
+             <img src={cassette} alt="cassete" />
         </div>
        
     )
