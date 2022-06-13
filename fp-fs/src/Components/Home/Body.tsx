1055abb328d3cbb04f2b608ab3879ab08037482a
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-13 21:27:13 +0200

    Colord Stripes in own Component

--- fp-fs/src/Components/Home/Body.tsx
+++ fp-fs/src/Components/Home/Body.tsx
@@ -5,17 +5,12 @@ import Reality from "../../Assets/Extended-Reality.png";
 import Smart from "../../Assets/Smart-Technologies.png";
 import Cassette1 from "../../Assets/suspiria.svg";
 import Cassette2 from "../../Assets/halloween.svg";
+import Stripes from "./Stripes";
 
 function Body(){
     return(
         <div className="bodyContainer">
-            <div className="stripesSide">
-                <div id="blue"></div>
-                <div id="red"></div>
-                <div id="orange"></div>
-                <div id="yellow"></div>
-                <div id="pink"></div>
-            </div>
+            <Stripes/>
             <div className="cassettesContainer">
                 <img className="cassette" src={Smart} alt="smart-technologies-cassette" />
                 <img className="cassette" src={Motion} alt="motion-cassette" />
---
+++ fp-fs/src/Components/Home/Stripes.tsx
@@ -0,0 +1,15 @@
+import "../../SCSS/Stripes.scss"
+
+function Stripes(){
+    return(
+        <div className="stripesSide">
+            <div id="blue"></div>
+            <div id="red"></div>
+            <div id="orange"></div>
+            <div id="yellow"></div>
+            <div id="pink"></div>
+        </div>
+    )
+}
+
+export default Stripes
