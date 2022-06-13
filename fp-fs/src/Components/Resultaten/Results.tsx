0e7419408fafd4e4719d991d1413a4bddc9975fe
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-13 17:31:48 +0200

    Component results

--- fp-fs/src/Components/Resultaten/Results.tsx
+++ fp-fs/src/Components/Resultaten/Results.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import Nav from "../Standard/Nav";
+import Footer from "../Standard/Footer";
+import "../../SCSS/Home.scss"
 
 function Results() {
 
   return (
-    <div className="resContainer">
-        <h1>Hallo</h1>
+    <div>
+      <nav><Nav/></nav>
+
+      <section className="navContainer">
+        <button className='stemmenBtn btn'>Genomineerde</button>
+        <button className='searchBtn btn'>Winnaars</button>
+      </section>
+
+      <footer><Footer/></footer>
     </div>
+    
   )
 }
 export default Results
