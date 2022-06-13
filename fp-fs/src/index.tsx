bcd84169f426c89184aed9dc55f27cbf266c4260
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-13 17:26:30 +0200

    Import Routing in index.tsx

--- fp-fs/src/index.tsx
+++ fp-fs/src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Results from './Components/Resultaten/Results';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"  element={<App/>}/>
+      <Route path="/results"  element={<Results/>}/>
+      <Route path="*" element={
+        <h1>Page not found</h1>
+      } />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
