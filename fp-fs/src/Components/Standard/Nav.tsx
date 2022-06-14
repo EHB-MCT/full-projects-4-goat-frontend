d044710b25b9e9dd0a201e5968bb970e496d2511
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-14 11:56:28 +0200

    Margin-top change + routing to right pages

--- fp-fs/src/Components/Standard/Nav.tsx
+++ fp-fs/src/Components/Standard/Nav.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../Assets/Final-Show-Logo.svg'
-import '../../SCSS/Button.scss'
-import '../../SCSS/Nav.scss'
+import Logo from '../../Assets/Final-Show-Logo.svg';
+import Animation from "../../Assets/finalshow_logo_animation_2.gif"
+import '../../SCSS/Button.scss';
+import '../../SCSS/Nav.scss';
 
 function Nav(){
     return(
         <div>
             <section className="navContainer">
-                <Link to="/"><img className='logoFinalGroot' src={Logo} alt="logo-final-show" /></Link>
+                <Link to="/" className='routingLinks'>
+                    <img className='logoFinalGroot static' src={Logo} alt="logo-final-show" />
+                    <img id="activeGif" src={Animation} alt="animation-logo"/>
+                </Link>
+
                 <div className='ButtonsNav'>
                     <button className='uploadenBtn btn'><Link to="/results" className='routingLinks'>Resultaten</Link></button>
                     <button className='stemmenBtn btn'>Uploaden</button>
@@ -24,8 +28,8 @@ function Nav(){
                     <div className='menu-button'></div>
                 </label>
                 <ul className="menu">
-                    <li><button className='btn btnMobile'>Genomineerden</button></li>
-                    <li><button className='btn btnMobile'>Winnaars</button></li>
+                    <Link to="/genomineerden" className='routingLinks'><li><button className='btn btnMobile'>Genomineerden</button></li></Link>
+                    <Link to="/winnaars" className='routingLinks'><li><button className='btn btnMobile'>Winnaars</button></li></Link>
                     <li><button className='btn btnMobile'>Uploaden</button></li>
                     <li><button className='btn btnMobile'>Stemming</button></li>
                 </ul>
