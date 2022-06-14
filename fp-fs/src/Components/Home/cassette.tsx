2a73b6be605bb50a872f8de1a1466279c58ccda0
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-14 15:24:07 +0200

    Added new cassette

--- fp-fs/src/Components/Home/cassette.tsx
+++ fp-fs/src/Components/Home/cassette.tsx
@@ -5,14 +5,16 @@ import side_2 from '../../Assets/side_2.png'
 import side_3 from '../../Assets/side_3.png'
 import side_4 from '../../Assets/side_4.png'
 import side_5 from '../../Assets/side_5.png'
+import side_6 from '../../Assets/side_6.png'
 
 export default function Cassette(props: any){
 
     const [cassette, setCassette] = useState(String)
+    const [cluster, setCluster] = useState(String)
 
     useEffect(() => {
 
-        var number = Math.floor(Math.random() * (5 - 1 + 1)) + 1
+        var number = Math.floor(Math.random() * (6 - 1 + 1)) + 1
 
         if(number === 1){
             setCassette(side_1)
@@ -24,8 +26,26 @@ export default function Cassette(props: any){
             setCassette(side_4)
         }   else if (number === 5) {
             setCassette(side_5)
+        }  else if (number === 6) {
+            setCassette(side_6)
         }
+
+        var numberCluster = Math.floor(Math.random() * (4 - 1 + 1)) + 1
+
+        if(numberCluster === 1){
+            setCluster("Web")
+        } else if (numberCluster === 2) {
+            setCluster("Motion")
+        } else if (numberCluster === 3) {
+            setCluster("Smart")
+        }  else if (numberCluster === 4) {
+            setCluster("Reality")
+        }
+
     }, [])
+
+
+
 
 
     return (
@@ -34,6 +54,10 @@ export default function Cassette(props: any){
             <h1 className='cassetteName'>{props.data.title}</h1>
             </div>
              <img src={cassette} alt="cassete" />
+            <div className='clusterColors'>
+                <div className={cluster}></div>
+                <div className={cluster}></div>
+            </div>
         </div>
        
     )
