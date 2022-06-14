b59478324b155ec02fcf91194c459c6c1fdb3ff7
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-14 15:21:40 +0200

    Displaying cassettes on pages

--- fp-fs/src/Components/Resultaten/Genomineerd.tsx
+++ fp-fs/src/Components/Resultaten/Genomineerd.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import "../../SCSS/Genomineerd.scss";
+import { finalWorkService } from "../../Services/finalWorkService";
+import Cassette from "../Home/cassette";
+import "../../SCSS/cassette.scss"
 
 function Genomineerd(){
+    const [finalWorks, setFinalWorks] = useState([])
+
+    useEffect(() => {
+        finalWorkService.fetchFinalWorks().then((Response) => {
+            var templateData =  Response.slice(0, 40);
+              setFinalWorks(templateData)
+          })
+    }, [])
+
+
     return(
-        <div className="genomineerdContainer">
-            <h1>Genomineerd</h1>
-        </div>
+        <div className="cassettesContainer">
+            {
+            finalWorks.map(x => {
+                return <Cassette data={x} key={x["id"]} ></Cassette>
+            })}     
+        </div>       
     )
 }
-
 export default Genomineerd
