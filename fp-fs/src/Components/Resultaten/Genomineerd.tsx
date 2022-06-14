1c1a39c8295a9f18a2a9325399b6d3d050774dc4
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-14 16:18:13 +0200

    Import assets + containers for genomineerden

--- fp-fs/src/Components/Resultaten/Genomineerd.tsx
+++ fp-fs/src/Components/Resultaten/Genomineerd.tsx
@@ -6,6 +6,9 @@ import "../../SCSS/cassette.scss";
 import "../../SCSS/Genomineerd.scss";
 
 import Motion from "../../Assets/Logos-Clusters/MOTION_Logo.svg"
+import WebApp from "../../Assets/Logos-Clusters/WEB&APP_LOGO.svg"
+import Reality from "../../Assets/Logos-Clusters/EXTENDEDREALITY_LOGO.svg"
+import Smart from "../../Assets/Logos-Clusters/SMARTTECHNOLOGY_Logo.svg"
 
 function Genomineerd(){
     const [finalWorks, setFinalWorks] = useState([])
@@ -19,15 +22,52 @@ function Genomineerd(){
 
 
     return(
-        <div className="webAppGenom">
-            <img className="logoImgcluster" src={Motion} alt="name-cluster-logo" />
-            <div className="cassettesContainer">
-                {
-                finalWorks.map(x => {
-                    return <Cassette data={x} key={x["id"]} ></Cassette>
-                })}
-            </div>     
-        </div>       
+        <div>
+            <div className="motionGenom">
+                <div className="motionImg">
+                    <img className="logoImgcluster" src={Motion} alt="name-cluster-logo" />
+                </div>
+                <div className="cassettesContainer">
+                    {
+                    finalWorks.map(x => {
+                        return <Cassette data={x} key={x["id"]} ></Cassette>
+                    })}
+                </div>     
+            </div>       
+            <div className="webAppGenom">
+                <div className="appImg">
+                    <img className="logoImgcluster" src={WebApp} alt="name-cluster-logo" />
+                </div>
+                <div className="cassettesContainer">
+                    {
+                    finalWorks.map(x => {
+                        return <Cassette data={x} key={x["id"]} ></Cassette>
+                    })}
+                </div>     
+            </div>       
+            <div className="realityGenom">
+                <div className="realityImg">
+                    <img className="logoImgcluster" src={Reality} alt="name-cluster-logo" />
+                </div>
+                <div className="cassettesContainer">
+                    {
+                    finalWorks.map(x => {
+                        return <Cassette data={x} key={x["id"]} ></Cassette>
+                    })}
+                </div>     
+            </div>       
+            <div className="smartGenom">
+                <div className="smartImg">
+                    <img className="logoImgcluster" src={Smart} alt="name-cluster-logo" />
+                </div>
+                <div className="cassettesContainer last">
+                    {
+                    finalWorks.map(x => {
+                        return <Cassette data={x} key={x["id"]} ></Cassette>
+                    })}
+                </div>     
+            </div>       
+        </div>
     )
 }
 export default Genomineerd
