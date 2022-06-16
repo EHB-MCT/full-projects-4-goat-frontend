fae7882892368c443534b934d3f2ca0bcffc7836
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-06-16 14:50:11 +0200

    Backgroundstripes

--- fp-fs/src/Components/Home/Stripes/BackgroundStripes.tsx
+++ fp-fs/src/Components/Home/Stripes/BackgroundStripes.tsx
@@ -4,19 +4,19 @@ export default function BackgroundStripes() {
   return (
     <div id="stripes-container">
       <div id="pink" className="stripe">
-        <p>Multimedia &</p>
-        <p>Creatieve Technologie</p>
+        <p className="stripeText">Multimedia &</p>
+        <p className="stripeText">Creatieve Technologie</p>
       </div>
       <div id="yellow" className="stripe">
         
       </div>
       <div id="orange" className="stripe">
-        <p>Livestream</p>
-        <p>24/06 19u</p>
+        <p className="stripeText">Livestream</p>
+        <p className="stripeText">24/06 19u</p>
       </div>
       <div id="red" className="stripe">
-        <p>Infodag</p>
-        <p>25/06 10u</p>
+        <p className="stripeText">Infodag</p>
+        <p className="stripeText">25/06 10u</p>
       </div>
       <div id="blue" className="stripe"></div>
     </div>
