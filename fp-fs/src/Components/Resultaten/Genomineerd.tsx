30b7e6e03651ec76f72ba0ef18a6396031144122
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-16 11:43:14 +0200

    Added timer for 'genomineerden'

--- fp-fs/src/Components/Resultaten/Genomineerd.tsx
+++ fp-fs/src/Components/Resultaten/Genomineerd.tsx
@@ -4,6 +4,7 @@ import { finalWorkService } from "../../Services/finalWorkService";
 import Cassette from "../Home/cassette";
 import "../../SCSS/cassette.scss";
 import "../../SCSS/Genomineerd.scss";
+import Countdown from 'react-countdown';
 
 import Motion from "../../Assets/Logos-Clusters/MOTION_Logo.svg"
 import WebApp from "../../Assets/Logos-Clusters/WEB&APP_LOGO.svg"
@@ -12,17 +13,22 @@ import Smart from "../../Assets/Logos-Clusters/SMARTTECHNOLOGY_Logo.svg"
 
 function Genomineerd(){
     const [finalWorks, setFinalWorks] = useState([])
+    const [date, setDate] = useState(String)
 
     useEffect(() => {
         finalWorkService.fetchFinalWorks().then((Response) => {
             var templateData =  Response.slice(0, 40);
               setFinalWorks(templateData)
           })
+
+          setDate(Date.now().toString())
+          console.log(Date.now())
     }, [])
 
 
     return(
         <div className="allClusters">
+            <Countdown date={new Date("2022-06-17T19:00:00+02:00")} />
             <div className="motionGenom">
                 <div className="motionImg">
                     <img className="logoImgcluster" src={Motion} alt="name-cluster-logo" />
