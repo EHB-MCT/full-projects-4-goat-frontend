b965d0fa9235d81a7cc964bc3b45faf4ca84dd9a
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-16 15:49:24 +0200

    Removed footer in detail page mobile

--- fp-fs/src/Components/Standard/Footer.tsx
+++ fp-fs/src/Components/Standard/Footer.tsx
@@ -1,7 +1,15 @@
+import { useLocation } from 'react-router-dom';
 import Erasmus from '../../Assets/logo-ehb-small.svg';
 import '../../SCSS/Footer.scss';
 
 function Footer(){
+
+    const pathName = useLocation().pathname
+
+    if(pathName === "/detail"){
+        return null
+    }
+
     return(
         <div className="footerContainer">
             <img className='footerImg' src={Erasmus} alt="erasmus-logo-black" />
