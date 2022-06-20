7f33c5afc0b979f0b1aaa351857cf068ae6be627
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-20 15:50:57 +0200

    Added routing for shared page

--- fp-fs/src/Components/Home/details/SharedDetail.tsx
+++ fp-fs/src/Components/Home/details/SharedDetail.tsx
@@ -2,15 +2,15 @@ import '../../../SCSS/sharedDetail.scss'
 import cross from '../../../Assets/cross_logo.png'
 import finalLogo from '../../../Assets/Final-Show-Logo.svg'
 import ehbLogo from '../../../Assets/logo-ehb-small-white.svg'
+import { Link } from 'react-router-dom'
 
 export default function SharedDetail(){
     return(
         <div id='backgroundRaster'>
             <img src={finalLogo} alt="" id='finalLogoSharedVersion'/>
             <img src={ehbLogo} alt="" id='ehbLogoSharedVersion'/>
-            <img src={cross} alt="closing-img" id="crossShared"/>
             <div id='cassetteBackgroundShared'>
-                <img src={cross} alt="closing-img" id="crossSharedWeb"/>
+            <Link to={"/"}><img src={cross} alt="closing-img" id="crossShared"/></Link>
             </div>
         </div>
     )
