dc33a725ad0e52181093cda4a73d3886a5349545
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-22 10:16:04 +0200

    Updated fetch url

--- fp-fs/src/Services/finalWorkService.tsx
+++ fp-fs/src/Services/finalWorkService.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 class FinalWorkService {
 
-    private readonly url = 'http://finalshowcasebackend.be/finalwork'
+    private readonly url = 'https://mighty-earth-63459.herokuapp.com/http://finalshowcasebackend.be/finalwork'
 
     public async fetchFinalWorks(){
         const response = await fetch(`${this.url}`);
