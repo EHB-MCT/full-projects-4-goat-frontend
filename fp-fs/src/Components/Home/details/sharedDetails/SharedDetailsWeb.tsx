667b4a5c8714d0afb8e2932b81b6ddcf99474086
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-22 10:17:53 +0200

    Added if statement to check if data is there

--- fp-fs/src/Components/Home/details/sharedDetails/SharedDetailsWeb.tsx
+++ fp-fs/src/Components/Home/details/sharedDetails/SharedDetailsWeb.tsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from "react"
+
 export default function SharedDetailWeb(props:any){
+    var [loading, setLoading] = useState(Boolean) 
+    const [cluster, setCluster] = useState(String)
+    const [clusterClass, setClusterClass] = useState(String)
+
+    useEffect(() => {
+        setLoading(false)
+        if(props.data !== undefined){
+            setLoading(true)
+
+            
+        if(props.data.cluster === "web" || props.data.cluster === "webApp"){
+            setCluster("Web en App")
+            setClusterClass("clusterNameMob Web")
+        } else if (props.data.cluster === "interactiveMotion" || props.data.cluster === "motion") {
+            setCluster("Motion")
+            setClusterClass("clusterNameMob Motion")
+        } else if (props.data.cluster === "digitalMaking" || props.data.cluster === "smartTechnologies") {
+            setCluster("Smart Technologies")
+            setClusterClass("clusterNameMob Smart")
+        }  else if (props.data.cluster === "alternativeReality" || props.data.cluster === "extendedReality") {
+            setCluster("Extended Reality")
+            setClusterClass("clusterNameMob Reality")
+        }
+        }
+    }, [loading, props.data])
+
     return (
-       <p>web</p>
+        <div className='detailPageContainer'>
+            {loading? 
+            <div>               
+                {props.data.title}
+            </div>
+           
+             
+            :
+      
+            <p>Loading...</p>
+        }
+         
+
+    </div>
     )
 }
