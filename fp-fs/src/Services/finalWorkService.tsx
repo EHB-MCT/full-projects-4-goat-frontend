662e0d215473c0e7590e0d81eabb0f6f86be259b
LieseCordeyn <liese.cordeyn@student.ehb.be>
2022-06-23 11:53:45 +0200

    Updated winners page

--- fp-fs/src/Services/finalWorkService.tsx
+++ fp-fs/src/Services/finalWorkService.tsx
@@ -2,19 +2,26 @@ import React from "react";
 
 class FinalWorkService {
   
-    private readonly url = 'https://mighty-earth-63459.herokuapp.com/http://finalshowcasebackend.be/finalwork'
+    private readonly url = 'https://mighty-earth-63459.herokuapp.com/http://finalshowcasebackend.be'
 
     public async fetchFinalWorks(){
-        const response = await fetch(`${this.url}`);
+        const response = await fetch(`${this.url}/finalwork`);
 
         return response.json();
     }
 
     public async fetchFinalWorkById(id: any){
-        const response = await fetch(`${this.url}/${id}`)
+        const response = await fetch(`${this.url}/finalwork/${id}`)
         
         return response.json();
     }
+
+    public async fetchAwards(){
+        const response = await fetch(`${this.url}/price`)
+        
+        return response.json();
+    }
+
 
 }
 
